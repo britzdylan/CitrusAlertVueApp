50eeb4f5c525a641b131de33285cf2eb4558be2a
Dylan Britz <britzdylan@gmail.com>
2023-07-06 11:49:08 +0200

    Added button Component

--- src/components/UI/button.vue
+++ src/components/UI/button.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <button :disabled="disabled || loading" class="btn">
+    <svg v-if="iconLeft" class="absolute left-1 top-6 bottom-1" width="36" height="36">
+      <use :xlink:href="`/tabler-sprite.svg#tabler-${loading ? 'loader' : iconLeft}`" />
+    </svg>
+    <slot />
+    <svg v-if="iconRight" class="absolute left-1 top-6 bottom-1" width="36" height="36">
+      <use :xlink:href="`/tabler-sprite.svg#tabler-${iconRight}`" />
+    </svg>
+  </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  iconLeft?: string
+  iconRight?: string
+  disabled?: boolean
+  loading?: boolean
+}>()
+</script>
 
 <style scoped></style>

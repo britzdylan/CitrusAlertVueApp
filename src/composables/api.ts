b78720e69853118500ea09204df633dfe9dc72af
Dylan Britz <britzdylan@gmail.com>
2023-07-11 09:12:41 +0200

    feat:updated api and stores

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -5,6 +5,13 @@ export function useApi() {
   const { decrypt } = useCrypto()
 
   const base_url = import.meta.env.VITE_API_URL
+  const getHeaders = (endpoint: string) => {
+    const encryptedKey = await get('api_key')
+    if (!encryptedKey) throw new Error('No API key found')
+    const token = await decrypt(encryptedKey, import.meta.env.VITE_SECRET)
+    const url = `${base_url}/${endPoint}`
+    return { url, token }
+  }
   const header = {
     Accept: 'application/vnd.api+json',
     'Content-Type': 'application/vnd.api+json'
@@ -12,10 +19,7 @@ export function useApi() {
   // use fetch to get data from the API
   const getData = async (endPoint: string): Promise<any> => {
     // @ts-ignore
-    const encryptedKey = await get('api_key')
-    if (!encryptedKey) throw new Error('No API key found')
-    const token = await decrypt(encryptedKey, import.meta.env.VITE_SECRET)
-    const url = `${base_url}/${endPoint}`
+    const { url, token } = getHeaders(endpoint)
 
     const response = await fetch(url, {
       headers: { ...header, Authorization: `Bearer ${token}` }
@@ -23,5 +27,40 @@ export function useApi() {
     return await response.json()
   }
 
-  return { getData }
+  const updateData = async (endPoint: string, data: any): Promise<any> => {
+    // @ts-ignore
+    const { url, token } = getHeaders(endpoint)
+
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: { ...header, Authorization: `Bearer ${token}` },
+      body: JSON.stringify(data)
+    })
+    return await response.json()
+  }
+
+  const postData = async (endPoint: string, data: any): Promise<any> => {
+    // @ts-ignore
+    const { url, token } = getHeaders(endpoint)
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { ...header, Authorization: `Bearer ${token}` },
+      body: JSON.stringify(data)
+    })
+    return await response.json()
+  }
+
+  const deleteData = async (endPoint: string): Promise<any> => {
+    // @ts-ignore
+    const { url, token } = getHeaders(endpoint)
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: { ...header, Authorization: `Bearer ${token}` }
+    })
+    return await response.json()
+  }
+
+  return { getData, postData, deleteData }
 }
